a585054f0443a49285ffce346cbf4b19e02288a7
Tanim Ahmed <114849222+TnzTanim@users.noreply.github.com>
2023-12-28 20:31:55 +0600

    g

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "./src/images/wood.jpg";
+import image from "../images/wood.jpg";
 
 const imageAltText = "wood background";
 
